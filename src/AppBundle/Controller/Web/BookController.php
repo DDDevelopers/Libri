4f661f7043c35caf1b93c01baffe20e8859285d1
Diar <diarselimi92@gmail.com>
2016-11-06 23:51:40 +0100

    Add to shelf
    Now the user can edit his shelf place for the specific book

--- src/AppBundle/Controller/Web/BookController.php
+++ src/AppBundle/Controller/Web/BookController.php
@@ -24,16 +24,9 @@ class BookController extends Controller
         $form = $this->createForm(BookType::class);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            if($book = $em->getRepository('AppBundle:Book')->findBookByTitle($form->getData()->getTitle())) {
-                $book->setPeacesInShelf($book->getPeacesInShelf() + 1);
-            }else{
-                /** @var Book $book */
-                $book = $form->getData();
-                $book->setPeacesInShelf(1);
-                $book->setUser($this->getUser());
-            }
+            $book = $form->getData();
             $em->persist($book);
             $em->flush();
 
@@ -53,7 +46,11 @@ class BookController extends Controller
      */
     public function viewAction(Book $book)
     {
-        $addToShelfForm = $this->createForm(BookToShelfType::class, new UserBookShelf(), [
+        $em = $this->getDoctrine()->getManager();
+        $addToShelfForm = $this->createForm(BookToShelfType::class, $em->getRepository(UserBookShelf::class)->findOneBy([
+            'user' => $this->getUser()->getId(),
+            'book' => $book->getId()
+        ]), [
             'action' => $this->generateUrl('save_book_in_shelf', ['id' => $book->getId()]),
             'method' => 'post'
         ]);
@@ -77,7 +74,7 @@ class BookController extends Controller
         $form = $this->createForm(BookType::class, $book);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $book = $form->getData();
             $em = $this->getDoctrine()->getManager();
             $em->persist($book);
